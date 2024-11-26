329f748f73bc4a92cb12eb20c3e282990ecf8601
Luc <lucht.23itb@vku.udn.vn>
2024-11-26 20:20:28 +0700

    Initial commit

---
+++ storage/framework/views/ae022420f3e141b80ea872d22e27f79d.php
@@ -0,0 +1,9 @@
+ 
+<?php $__env->startSection('contact'); ?> 
+
+<h3>Trang lien he</h3> 
+<p>Nội dung trang lien he!!!</p> 
+</div> 
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\test-web-laravel\resources\views/pages/contact.blade.php ENDPATH**/ ?>
